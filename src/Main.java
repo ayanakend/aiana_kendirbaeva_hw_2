276e73aec9e342ef9333e426e2599028d1dc7626
ayakend <ayakend@gmail.com>
2022-10-05 20:20:02 +0300

    final version

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        String canWeGoForAWalk = canWeGoForAWalk(8, 18);
-        String canWeGoForAWalk1 = canWeGoForAWalk(24, 37);
-        String canWeGoForAWalk2 = canWeGoForAWalk(50, 17);
-        String canWeGoForAWalk3 = canWeGoForAWalk(38, 28);
-        String canWeGoForAWalk4 = canWeGoForAWalk(3, -18);
-        String canWeGoForAWalk5 = canWeGoForAWalk(generateRandomAge(), 30);
+        String canWeGoForWalk = canWeGoForWalk(8, 18 );
+        System.out.println(canWeGoForWalk);
+        String canWeGoForWalk1 = canWeGoForWalk(24, 37);
+        System.out.println(canWeGoForWalk1);
+        String canWeGoForWalk2 = canWeGoForWalk(50, 17);
+        System.out.println(canWeGoForWalk2);
+        String canWeGoForWalk3 = canWeGoForWalk(38, 28);
+        System.out.println(canWeGoForWalk3);
+        String canWeGoForWalk4 = canWeGoForWalk(3, -18);
+        System.out.println(canWeGoForWalk4);
+        String canWeGoForWalk5 = canWeGoForWalk(generateRandomAge(), 30);
+        System.out.println(canWeGoForWalk5);
     }
 
-    public static String canWeGoForAWalk(int age , int temperature) {
+    public static String canWeGoForWalk(int age , int temperature) {
         if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
-            System.out.println ("Можно идти гулять");
+            return  ("Можно идти гулять");
         } else if (age < 20 && temperature >= 0 && temperature <= 28) {
-            System.out.println ("Можно идти гулять");
+            return  ("Можно идти гулять");
         } else if (age > 45 && temperature >= -10 && temperature <= 25) {
-            System.out.println("Можно идти гулять");
+            return ("Можно идти гулять");
         } else {
-            System.out.println("Оставайтесь дома");
+            return ("Оставайтесь дома");
         }
-        return "The String is returned by using Return Statement";
     }
 
     public static int generateRandomAge(){
