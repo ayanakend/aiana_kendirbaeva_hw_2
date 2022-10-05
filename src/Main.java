ec3db34025a813fb1fafc591fd5a6465755f5519
ayakend <ayakend@gmail.com>
2022-10-05 18:25:38 +0300

    это всё что я смогла найти и сделать

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         String canWeGoForAWalk = canWeGoForAWalk(8, 18);
-        String canWeGoForAWalk1 = canWeGoForAWalk(24, 26);
+        String canWeGoForAWalk1 = canWeGoForAWalk(24, 37);
         String canWeGoForAWalk2 = canWeGoForAWalk(50, 17);
         String canWeGoForAWalk3 = canWeGoForAWalk(38, 28);
         String canWeGoForAWalk4 = canWeGoForAWalk(3, -18);
@@ -10,15 +10,15 @@ public class Main {
 
     public static String canWeGoForAWalk(int age , int temperature) {
         if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30) {
-            System.out.println("Можно идти гулять");
+            System.out.println ("Можно идти гулять");
         } else if (age < 20 && temperature >= 0 && temperature <= 28) {
-            System.out.println("Можно идти гулять");
+            System.out.println ("Можно идти гулять");
         } else if (age > 45 && temperature >= -10 && temperature <= 25) {
             System.out.println("Можно идти гулять");
         } else {
             System.out.println("Оставайтесь дома");
         }
-        return "Можно идти гулять";
+        return "The String is returned by using Return Statement";
     }
 
     public static int generateRandomAge(){
